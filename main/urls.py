d4c626e3eb1e90c0ccdcd59aa76ca35c1245f0f4
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-08-05 00:29:24 -0700

    changes 3

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns=[
-    path('homepage',views.homepage,name="homepage"),
-    path('output',views.output,name="output"),
+    path('/input',views.homepage,name="homepage"),
+    path('/output',views.output,name="output"),
     
 ]
--- proactive_2/urls.py
+++ proactive_2/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls'))
+    path('www.proactiveforher.com/toolsweight-gain-assessment',include('main.urls')),
+   
 ]
 
 if settings.DEBUG: 
